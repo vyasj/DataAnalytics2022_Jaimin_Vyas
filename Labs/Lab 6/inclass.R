880118dd4702323d100547f0f28054700ec8cb59
Jaimin Vyas <vyasj@github.com>
2022-10-21 15:42:30 -0400

    finalized lab 6

--- Labs/Lab 6/inclass.R
+++ Labs/Lab 6/inclass.R
@@ -13,3 +13,27 @@ CooksDistance <- cooks.distance(model1)
 round(CooksDistance, 5)
 sort(round(CooksDistance, 5))
 
+library(ISLR)
+head(Hitters)
+dim(Hitters)
+is.na(Hitters)
+
+HittersData <- na.omit(Hitters)
+dim(HittersData)
+glimpse(HittersData)
+head(HittersData)
+
+SalaryPredictModel1 <- lm(Salary ~., data=HittersData)
+summary(SalaryPredictModel1)
+
+cooksD <- cooks.distance(SalaryPredictModel1)
+influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm=T)))]
+influential
+
+names_of_influential <- names(influential)
+names_of_influential
+outliers <- HittersData[names_of_influential,]
+Hitters_Without_Outliers <- HittersData %>% anti_join(outliers)
+
+SalaryPredictModel2 <- lm(Salary ~., data=Hitters_Without_Outliers)
+summary(SalaryPredictModel2)
